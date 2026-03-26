4b2cc88d29755e5dc29bedd7cb5bc1633ebbf8ae
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2026-03-26 16:00:31 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -323,85 +323,22 @@
             <div class="container">
                 <ul class="footer-nav-list">
                     <li class="footer-nav-item">
-                        <h2 class="nav-title">Popular Categories</h2>
+                        <h2 class="nav-title">Information</h2>
                     </li>
                     <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Fashion</a>
+                        <a href="{{ route('makeup') }}" class="footer-nav-link">Makeup</a>
                     </li>
                     <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Electronic</a>
+                        <a href="{{ route('box') }}" class="footer-nav-link">Papan Bunga - Ucapan</a>
                     </li>
                     <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Cosmetic</a>
+                        <a href="{{ route('attire') }}" class="footer-nav-link">Sewa Baju - Attire</a>
                     </li>
                     <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Health</a>
+                        <a href="{{ route('farm') }}" class="footer-nav-link">Kebun</a>
                     </li>
                     <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Watches</a>
-                    </li>
-                </ul>
-
-                <ul class="footer-nav-list">
-                    <li class="footer-nav-item">
-                        <h2 class="nav-title">Products</h2>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Prices drop</a>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">New products</a>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Best sales</a>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Contact us</a>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Sitemap</a>
-                    </li>
-                </ul>
-
-                <ul class="footer-nav-list">
-                    <li class="footer-nav-item">
-                        <h2 class="nav-title">Our Company</h2>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Delivery</a>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Legal Notice</a>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Terms and conditions</a>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">About us</a>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Secure payment</a>
-                    </li>
-                </ul>
-
-                <ul class="footer-nav-list">
-                    <li class="footer-nav-item">
-                        <h2 class="nav-title">Services</h2>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Prices drop</a>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">New products</a>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Best sales</a>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Contact us</a>
-                    </li>
-                    <li class="footer-nav-item">
-                        <a href="#" class="footer-nav-link">Sitemap</a>
+                        <a href="{{ route('link') }}" class="footer-nav-link">Link Affiliate</a>
                     </li>
                 </ul>
 
@@ -414,21 +351,21 @@
                             <ion-icon name="location-outline"></ion-icon>
                         </div>
                         <address class="content">
-                            419 State 414 Rte
-                            Beaver Dams, New York(NY), 14812, USA
+                            Jl. Kopral Hanapia No. 122 13/02, Serasan Jaya, Sekayu
+                            <br> Musi Banyuasin, Sumatera Selatan
                         </address>
                     </li>
                     <li class="footer-nav-item flex">
                         <div class="icon-box">
                             <ion-icon name="call-outline"></ion-icon>
                         </div>
-                        <a href="tel:+607936-8058" class="footer-nav-link">(607) 936-8058</a>
+                        <a href="tel:+607936-8058" class="footer-nav-link">+62 851-6689-4512</a>
                     </li>
                     <li class="footer-nav-item flex">
                         <div class="icon-box">
                             <ion-icon name="mail-outline"></ion-icon>
                         </div>
-                        <a href="mailto:example@gmail.com" class="footer-nav-link">example@gmail.com</a>
+                        <a href="mailto:awikyusmin@gmail.com" class="footer-nav-link">awikyusmin@gmail.com</a>
                     </li>
                 </ul>
 
