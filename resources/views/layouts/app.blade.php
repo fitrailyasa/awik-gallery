3acab2595f3d4e1d0ae504294d126b9d65af0c57
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2026-03-26 17:56:53 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title') - Awik Gallery</title>
+    <title>{{ $title ?? '' }} - Awik Gallery</title>
 
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('assets/css/style-prefix.css') }}" type="text/css" media="all" />
